4ea0ed42daa69c385e9728bd6f963e41f73eee62
StrangRoll <93737752+StrangRoll@users.noreply.github.com>
2022-08-21 12:39:51 +0300

    Clean Code Task 2

--- Task.cs
+++ Task.cs
@@ -2,4 +2,15 @@
 
 namespace Task
 {
+    class Program
+    {
+        public static int FindElement(int[] array, int element)
+        {
+            for (int i = 0; i < array.Length; i++)
+                if (array[i] == element)
+                    return i;
+
+            return -1;
+        }
+    }
 }
