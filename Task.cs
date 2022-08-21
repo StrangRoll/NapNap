2724a52ec4890b45f3e4fc57e7eb5a1be14f4ea2
StrangRoll <93737752+StrangRoll@users.noreply.github.com>
2022-08-21 12:49:01 +0300

    Clean Code Task 7

--- Task.cs
+++ Task.cs
@@ -2,4 +2,21 @@
 
 namespace Task
 {
+    class Program
+    {
+        public static void CreateNewObject()
+        {
+            //Создание объекта на карте
+        }
+
+        public static void RandomizeChance()
+        {
+            _chance = Random.Range(0, 100);
+        }
+
+        public static int GetSalary(int hoursWorked)
+        {
+            return _hourlyRate * hoursWorked;
+        }
+    }
 }
