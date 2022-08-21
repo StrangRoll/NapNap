519790e0779400450c3cdb6feb01831c456c49d1
StrangRoll <93737752+StrangRoll@users.noreply.github.com>
2022-08-21 12:54:06 +0300

    Clean Code Task 11

--- Task.cs
+++ Task.cs
@@ -2,4 +2,16 @@
 
 namespace Task
 {
+    class Program
+    {
+        public void Enable()
+        {
+            _effects.StartEnableAnimation();
+        }
+
+        public void Disable()
+        {
+            _pool.Free(this);
+        }
+    }
 }
