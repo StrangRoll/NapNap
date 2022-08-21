379d655398397f6137d14832896f5a9916bd84b1
StrangRoll <93737752+StrangRoll@users.noreply.github.com>
2022-08-21 12:38:19 +0300

    Clean Code Task 1

--- Task.cs
+++ Task.cs
@@ -2,4 +2,16 @@
 
 namespace Task
 {
+    class Program
+    {
+        public static int NumberClamp(int a, int b, int c)
+        {
+            if (a < b)
+                return b;
+            else if (a > c)
+                return c;
+            else
+                return a;
+        }
+    }
 }
