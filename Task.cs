a77f4f6068c76812ac0bbbc2f12a824b7246589a
StrangRoll <93737752+StrangRoll@users.noreply.github.com>
2022-08-21 12:50:25 +0300

    Clean Code Task 8

--- Task.cs
+++ Task.cs
@@ -2,4 +2,37 @@
 
 namespace Task
 {
+    class Player
+    {
+        public string Name { get; private set; }
+        public int Age { get; private set; }
+
+        public void Move()
+        {
+            //Do move
+        }
+
+        public void Attack()
+        {
+            //attack
+        }
+
+        public bool IsReloading()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    class Movement
+    {
+        public float DirectionX { get; private set; }
+        public float DirectionY { get; private set; }
+        public float Speed { get; private set; }
+    }
+
+    class Weapon
+    {
+        public float Cooldown { get; private set; }
+        public int Damage { get; private set; }
+    }
 }
